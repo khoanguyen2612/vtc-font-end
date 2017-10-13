0daae00f62059c7dc650508e72c16dd4d6fac74e
Bùi Thị Diệu <buithidieu@Buis-MacBook-Air.local>
2017-10-13 14:48:43 +0700

    test

--- front-end-website/app/Model/Account.php
+++ front-end-website/app/Model/Account.php
@@ -5,5 +5,80 @@ App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
 class Account extends AppModel {
     public $name = "Account";
+	public $validate = array(
+            'nickname' =>array(
+                'too long'=>array(
+                    'rule' => array('between', 5, 32),
+                    'message' => 'Tài khoản phải lớn 5 ký tự'
+                ),
+                'not empty' => array(
+                    'rule' => 'notBlank',
+                    'message' => 'Tài khoản không được để trống'
+                ),
+                'duplicate nickname' => array(
+                    'rule'=>'isUnique',
+                    'message' => 'Tài khoản này đã có người dùng'
+                )
+            ),
+            'original_password' => array(
+                'too long' => array(
+                    'rule' => array('between', 6, 32),
+                    'message' => 'Mật khẩu phải từ 6 ký tự đến 32 ký tự'
+                ),
+                'not empty' => array(
+                    'rule' => 'notBlank',
+                    'message' => 'Mật khẩu không được để trống'
+                ),
+                'Match Password' => array(
+                    'rule' => 'matchPasswords',
+                    'message' => 'Không trùng khớp mật khẩu'
+                )
+            ),
+            'confirm_password' => array(
+                'too long' => array(
+                    'rule' => array('between', 6, 32),
+                    'message' => 'Nhập 6 - 32 ký tự'
+                ),
+                'not empty' => array(
+                    'rule' => 'notBlank',
+                    'message' => 'Vui lòng xác nhận mật khẩu'
+                )
+            ),
+            'email' => array(
+                'valid email' => array(
+                    'rule' => 'email',
+                    'message' => 'Nhập địa chỉ email hợp lệ'
+                ),
+                'duplicate email' => array(
+                    'rule'=>'isUnique',
+                    'message' => 'email đã có người sử dụng'
+                )
+            ),
+            'name' => array(
+                'not empty' => array(
+                    'rule' => 'notBlank',
+                    'message' => 'Vui lòng nhập họ tên'
+                )
+            )
+        );
+
+	public function matchPasswords($data){
+        if($this->data['Account']['original_password']==$this->data['Account']['confirm_password']){
+            return true;
+        }
+        $this->invalidate('confirm_password', 'Không trùng khớp mật khẩu');
+        return false;
+    }
+
+	// public function beforeSave($options = array()) {
+ //        if (isset($this->data[$this->alias]['original_password'])) {
+ //            $passwordHasher = new BlowfishPasswordHasher();
+ //            $this->data[$this->alias]['original_password'] = $passwordHasher->hash(
+ //                    $this->data[$this->alias]['original_password']
+ //            );
+ //        }
+ //        return true;
+ //    }
+
 
 }
