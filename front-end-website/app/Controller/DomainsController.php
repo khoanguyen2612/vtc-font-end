8bb8cd1262c9a8e1351c363ffac4ea4559a392e6
Thang Do <dohuuthang0607@gmail.com>
2017-10-17 14:02:09 +0700

    dang ky ten mien

--- front-end-website/app/Controller/DomainsController.php
+++ front-end-website/app/Controller/DomainsController.php
@@ -58,7 +58,75 @@
 
 			}
 		}
-        
+        public function RegisterDomain()
+        {
+        	$sql = "select * from product_price";
+			$data = $this->Domain->query($sql);
+			$this->set('data',$data);
+        	if($this->request->is('post'))
+        	{
+        		$request = ($this->request->data);
+				echo $request['add-domain'];
+				$this->set('request',$request);	
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+				$sql1 = "select * from product_price";
+				$data1 = $this->Domain->query($sql1);	
+				$i=0;
+				foreach($data1 as $item)
+				{
+					$test = $request['add-domain'].$item['product_price']['product_name'];
+
+					
+//--------------------------------------------------------------------------------------------------------------------------------------
+					$Login = array("email" => "phuongnt6@vtc.vn", "password" => "Phuongnt6");
+					$ch = curl_init("https://dms.inet.vn/api/sso/v1/user/signin");
+
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_POST, true);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($Login));
+
+					$output = curl_exec($ch);
+					$output = json_decode($output, true);
+					$token =  ($output['session']['token']);
+					curl_close($ch);
+					
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+
+					$checkDomain = array(  "name" => $test, "registrar" => "inet");
+	
+					$data_string = json_encode($checkDomain);   
+
+
+					$ch = curl_init("https://dms.inet.vn/api/rms/v1/domain/checkavailable");
+
+					curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+					//curl_setopt($ch, CURLOPT_POST, true);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+					curl_setopt($ch, CURLOPT_HTTPHEADER, array
+						(                                                                          
+							'Content-Type:application/json; charset=UTF-8',  
+							'token: '.$token                                                                             
+					    )                                                                       
+					);       
+
+					$output = curl_exec($ch);
+					$output = json_decode($output, true);
+					$output1[$i]=$output;
+					$i++;
+					
+
+				 	
+				}
+
+				$this->set('output1',$output1);
+				curl_close($ch);
+
+        	}
+
+        }
 	}
 
 ?>
--- front-end-website/app/View/Elements/home/header.ctp
+++ front-end-website/app/View/Elements/home/header.ctp
@@ -6,7 +6,7 @@
 						<div class="btn-group" role="group">
 							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								LANGUAGE
-								<img src="img/lang-icon.png">
+								<img src="../app/webroot/img/lang-icon.png">
 								<span class="caret"></span>
 							</button>
 							<ul class="dropdown-menu">
@@ -21,31 +21,31 @@
 						<ul class="list-inline">
 							<li style="height: 41px;">
 								<a href="" class="phone">
-									<img src="img/phone-icon.png">
+									<img src="../app/webroot/img/phone-icon.png">
 									(04) 4450 5566
 								</a>
 							</li>
 							<li>
 								<a href="">
-									<img src="img/email-icon.png">
+									<img src="../app/webroot/img/email-icon.png">
 									cloud.info@vtc.vn 
 								</a>
 							</li>
 							<li>
 								<a href="">
-									<img src="img/suppot-icon.png">
+									<img src="../app/webroot/img/suppot-icon.png">
 									Hỗ trợ
 								</a>
 							</li>
 							<li>
 								<a href="<?php echo $this->Html->url(array( 'controller' => 'members', 'action' => 'login' ), true); ?>">
-									<img src="img/user-icon.png">
+									<img src="../app/webroot/img/user-icon.png">
 									Đăng nhập
 								</a>
 							</li>
 							<li>
 								<a href="">
-									<img src="img/cart-icon.png">
+									<img src="../app/webroot/img/cart-icon.png">
 									0
 								</a>
 							</li>
@@ -64,7 +64,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span> 
 				</button>
-				<a href="index.html"><img src="img/vtc-logo.png"></a>
+				<a href="index.html"><img src="../app/webroot/img/vtc-logo.png"></a>
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
 				<ul class="nav navbar-nav navbar-right">
