e7962c336d38427e7b0f398f6ec725ac65ae8c62
Thang Do <dohuuthang0607@gmail.com>
2017-11-01 11:50:24 +0700

    danh sach tin tuc va hien thi noi dung chi tiet

--- front-end-website/app/Controller/NewsController.php
+++ front-end-website/app/Controller/NewsController.php
@@ -1,11 +1,36 @@
 <?php 
 	class NewsController extends AppController
 	{
-		public function Notificion_maintain()
+		public $helpers = array('Paginator','Html');
+		//public $paginate = array();
+		public $use = array
+				(
+					'News',
+				);
+		
+		public function News_menulist()
 		{
-			
-		}
 
+			$top_new=$this->News->find('all', array(
+				'order' => array( 'id' => 'DESC' ),
+				'limit' => 1,
+			));
+			//pr($top_new); die;
+			$this->set('top_new',$top_new);
+			$this->Paginator->settings = array(
+				'order' => array( 'id' => 'DESC' ),
+				'limit' => 4,
+			);
+			$data = $this->Paginator->paginate('News');
+			$this->set(compact('data'));
+		}
+		public function Notificion_maintain(int $id = null)
+		{
+			$data=$this->News->find('all', array(
+				'conditions' => array( 'id LIKE' => "$id" )
+			));
+			$this->set('data',$data);
+		}
 
 	}
 
