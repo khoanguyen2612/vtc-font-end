d92a7583d6b375c71ccc5c07f22688bc8a49e0f3
Thang Do <dohuuthang0607@gmail.com>
2017-11-06 11:05:37 +0700

    add tab thong bao muc News

--- front-end-website/app/Controller/NewsController.php
+++ front-end-website/app/Controller/NewsController.php
@@ -12,11 +12,21 @@
 		{
 
 			$top_new=$this->News->find('all', array(
+				'conditions' => array( 'new_news_flg LIKE' => "0" ),
 				'order' => array( 'id' => 'DESC' ),
 				'limit' => 1,
 			));
 			//pr($top_new); die;
 			$this->set('top_new',$top_new);
+			//---------------------------------------
+			$top_new1=$this->News->find('all', array(
+				'conditions' => array( 'new_news_flg LIKE' => "1" ),
+				'order' => array( 'id' => 'DESC' ),
+				'limit' => 1,
+			));
+			//pr($top_new); die;
+			$this->set('top_new1',$top_new1);
+			//---------------------------------------
 			$this->Paginator->settings = array(
 				'conditions' => array( 'new_news_flg LIKE' => "0" ),
 				'order' => array( 'id' => 'DESC' ),
@@ -24,7 +34,17 @@
 			);
 			$data = $this->Paginator->paginate('News');
 			$this->set(compact('data'));
-		}
+
+			//-----------------------------------------
+
+			$this->Paginator->settings = array(
+					'conditions' => array( 'new_news_flg LIKE' => "1" ),
+					'order' => array( 'id' => 'DESC' ),
+					'limit' => 4,
+				);
+				$data1 = $this->Paginator->paginate('News');
+				$this->set(compact('data1'));
+			}
 		public function notificion_maintain($id = null)
 		{
 			$data=$this->News->find('all', array(
